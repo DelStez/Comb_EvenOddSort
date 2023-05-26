f7c2061741a663133f133922b4f0b50317c4310f
DelStez <kandakova0002@gmail.com>
2023-05-26 14:08:39 +0500

    Update based acts

---
+++ CombEvenOddSort/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace CombEvenOddSort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(278, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 152);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(341, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "START";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.start_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(87, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(185, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Загрузить";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 80);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(341, 66);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 23);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Исходник";
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(359, 7);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(353, 380);
+            this.zedGraphControl1.TabIndex = 9;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(185, 216);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox4.Size = new System.Drawing.Size(168, 96);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(11, 216);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(168, 96);
+            this.textBox3.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(11, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 23);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Чет-Нечет";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(185, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 23);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Расческа";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(11, 318);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(168, 23);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Время:   ";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(11, 341);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(168, 23);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Кол-во перестановок: ";
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(11, 364);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(168, 23);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Кол-во сравнений: ";
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(185, 364);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(168, 23);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Кол-во сравнений: ";
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(185, 341);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(168, 23);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Кол-во перестановок: ";
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(185, 318);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(168, 23);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Время:   ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(721, 392);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "CombEvenSort";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label label10;
+
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+
+        private System.Windows.Forms.Label label5;
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.TextBox textBox4;
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+
+        #endregion
+    }
+}
---
+++ CombEvenOddSort/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CombEvenOddSort
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+        }
+        string copyFileName1 = "CombSort.txt";
+        string copyFileName2 = "EvenSort.txt";
+        int swapping = 0; int comprassion = 0;
+        public List<double> numb = new List<double>();
+        
+        
+        private void Load_Click(object sender, EventArgs e)
+        {
+            Cleaning();
+            button2.Enabled = true;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.Filter = "Текстовый файл (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string fileContent = File.ReadAllText(openFileDialog1.FileName);
+                    CheckingAllNumber(openFileDialog1.FileName);
+                    button2.Enabled = true;
+                    //создание копии файла
+                    using (StreamReader reader = new StreamReader(openFileDialog1.FileName))
+                    {
+                        
+                        // Открыть два новых файла для записи
+                        using (StreamWriter writer1 = new StreamWriter(copyFileName1))
+                        using (StreamWriter writer2 = new StreamWriter(copyFileName2))
+                        {
+                            // Считывать поочередно каждую строку из исходного файла и записывать ее в оба новых файла
+                            string line;
+                            while ((line = reader.ReadLine()) != null)
+                            {
+                                writer1.WriteLine(line);
+                                writer2.WriteLine(line);
+                            }
+                        }
+                    }
+                    textBox1.Text = openFileDialog1.FileName;
+                    textBox2.Text = fileContent;
+                    
+                    
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show("Ошибка чтения файла: " + ex.Message);
+                }
+            }
+        }
+        private bool CheckingAllNumber(string fileName)
+        {
+            bool isDigitOnly = true;
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                int nextChar = reader.Read();
+                while (nextChar != -1)
+                {
+                    if (!char.IsDigit((char)nextChar) && nextChar != ' ' && nextChar != '.')
+                    {
+                        isDigitOnly = false;
+                        break;
+                    }
+                    nextChar = reader.Read();
+                }
+            }
+            if (!isDigitOnly)
+            {
+                MessageBox.Show("Файл содержит недопустимые символы.");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private List<double> preStart(string FileName)
+        {
+            //
+            string content = File.ReadAllText(copyFileName1).Replace("\r\n", "");
+            string[] texfile = content.Split(new string[] { "\n", " ", "\0" }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string Line in texfile)
+            {
+                double[] temp = Line.Split(new char[] { '\n', ' ', '\r' }).Select(double.Parse).ToArray();
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    numb.Add(temp[i]);
+                }
+            }
+
+            return numb;
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            List<double> numb= preStart(copyFileName1);
+            
+            swapping = 0; comprassion = 0;
+            var Start = DateTime.Now;
+            List<double> result = сombSort(numb);
+            var spendtime = DateTime.Now - Start;
+            
+            textBox3.Text = "";
+            foreach (int n in result)
+            {
+                textBox3.Text += n.ToString() + "\r\n";
+            } 
+            label5.Text = "Время:   " + spendtime.TotalMilliseconds + " мс";
+            label6.Text = "Кол-во перестановок: " + swapping.ToString();;
+            label7.Text = "Кол-во сравнений: " + comprassion.ToString();
+            
+            numb = preStart(copyFileName2);
+            swapping = 0; comprassion = 0;
+            Start = DateTime.Now;
+            result = OESort(numb);
+            spendtime = DateTime.Now - Start;
+            textBox4.Text = "";
+            foreach (int n in result)
+            {
+                textBox4.Text += n.ToString() + "\r\n";
+            }
+            label10.Text = "Время:   " + spendtime.TotalMilliseconds + " мс";
+            label9.Text = "Кол-во перестановок: " + swapping.ToString();;
+            label8.Text = "Кол-во сравнений: " + comprassion.ToString();
+
+        }
+        
+        private void Cleaning()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            numb.Clear();
+            label5.Text = "Время:   ";
+            label6.Text = "Кол-во перестановок:";
+            label7.Text = "Кол-во сравнений:";
+            label10.Text = "Время:   ";
+            label9.Text = "Кол-во перестановок:";
+            label8.Text = "Кол-во сравнений:";
+
+        }
+        public  List<double> сombSort( List<double> array)
+        {
+            int length = array.Count;
+            int gap = length;
+            double shrink = 1.3;
+            bool sorted = false;
+
+            while (!sorted)
+            {
+                gap = (int)(gap / shrink);
+                if (gap <= 1)
+                {
+                    comprassion++;
+                    gap = 1;
+                    sorted = true;
+                }
+
+                int i = 0;
+                while (i + gap < length)
+                {
+                    if (array[i] > array[i + gap])
+                    {
+                        comprassion++;
+                        swapping++;
+                        double temp = array[i];
+                        array[i] = array[i + gap];
+                        array[i + gap] = temp;
+                        sorted = false;
+                    }
+                    i++;
+                }
+            }
+
+            return array;
+        }
+        
+        public List<double> OESort(List<double> array)
+        {
+            bool sorted = false;
+
+            while (sorted == false)
+            {
+                sorted = true;
+
+                for (int i = 1; i <= array.Count - 2; i = i + 2)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        comprassion++;
+                        swapping++;
+                        double temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+                        sorted = false;
+                    }
+                }
+                
+            }
+
+            return array;
+        }
+    }
+}
